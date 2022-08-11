8884f9cf07cf6a550ff583346fcc832d4ca91346
Chinmay Jathar <96647640+Batman2002@users.noreply.github.com>
2022-08-11 14:30:07 +0000

    created array and dict defined.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 def calculate(list):
-  if len(list)<9:
-    retrun"List must contain nine numbers."
+  if len(list)!=9:
+    raise ValueError("List must contain nine numbers.")
+  else:
+    arr1=[]
+    arr1.append(list[0:3])
+    arr1.append(list[3:6])
+    arr1.append(list[6:])
+    calculations={}
+    
 
-
-
-    return calculations
+  calculations=0
+  return calculations
